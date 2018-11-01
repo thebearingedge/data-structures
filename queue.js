343041dfffdf36bdf33f3930108d94f63c1a75d9
thebearingedge <contact@timdav.is>
2018-11-01 15:30:29 -0700

    Implement a queue.

---
+++ queue.js
@@ -0,0 +1,28 @@
+import LinkedList from './linked-list'
+
+export default class Queue {
+  constructor(...values) {
+    this._items = new LinkedList(...values)
+  }
+  get size() {
+    return this._items.length
+  }
+  peek() {
+    return this._items.head
+  }
+  enqueue(value) {
+    this._items.append(value)
+    return this
+  }
+  dequeue() {
+    if (this._items.length === 0) {
+      throw new RangeError('Cannot dequeue from an empty Queue')
+    }
+    const { head: item } = this._items
+    this._items.delete(0)
+    return item
+  }
+  toArray() {
+    return this._items.toArray()
+  }
+}
---
+++ queue.test.js
@@ -0,0 +1,85 @@
+import { expect } from 'chai'
+import Queue from './queue'
+
+describe('Queue', () => {
+
+  describe('size', () => {
+
+    context('when the queue is empty', () => {
+
+      it('returns 0', () => {
+        const queue = new Queue()
+        expect(queue.size).to.equal(0)
+      })
+
+    })
+
+    context('when the queue is not empty', () => {
+
+      it('returns the number of items passed to the Queue', () => {
+        const queue = new Queue(1, 2, 3, 4)
+        expect(queue.size).to.equal(4)
+      })
+
+    })
+
+  })
+
+  describe('peek()', () => {
+
+    context('when the queue is empty', () => {
+
+      it('returns undefined', () => {
+        const queue = new Queue()
+        expect(queue.peek()).to.equal(undefined)
+      })
+
+    })
+
+    context('when the queue not empty', () => {
+
+      it('returns the first item in the queue', () => {
+        const queue = new Queue(4, 5, 6, 7)
+        expect(queue.peek()).to.equal(4)
+      })
+
+    })
+
+  })
+
+  describe('enqueue(value)', () => {
+
+    it('adds a value to the end of the queue', () => {
+      const queue = new Queue().enqueue(7)
+      expect(queue.size).to.equal(1)
+      expect(queue.peek()).to.equal(7)
+      expect(queue.toArray()).to.deep.equal([7])
+    })
+  })
+
+  describe('dequeue()', () => {
+
+    context('when the queue is empty', () => {
+
+      it('throws a RangeError', () => {
+        const queue = new Queue()
+        expect(() => queue.dequeue()).to.throw(RangeError)
+      })
+    })
+
+    context('when the queue is not empty', () => {
+
+      it('removes and returns the first item in the queue', () => {
+        const queue = new Queue(1, 2, 3, 4)
+        const item = queue.dequeue()
+        expect(item).to.equal(1)
+        expect(queue.size).to.equal(3)
+        expect(queue.peek()).to.equal(2)
+        expect(queue.toArray()).to.deep.equal([2, 3, 4])
+      })
+
+    })
+
+  })
+
+})
