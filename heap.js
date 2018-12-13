c16ecaec75243d8643b646cb5a3f1bf886e63b09
thebearingedge <contact@timdav.is>
2018-12-12 19:18:03 -0800

    Add helper methods to heap.

--- heap.js
+++ heap.js
@@ -1,16 +1,32 @@
-class Heap {
+export default class Heap {
   constructor() {
-    this.array = []
+    this._items = []
   }
-
+  _getLeftChildIndex(index) {
+    return (index + 1) * 2 - 1
+  }
+  _getRightChildIndex(index) {
+    return (index + 1) * 2
+  }
+  _getParentIndex(index) {
+    return Math.floor((index + 1) / 2) - 1
+  }
+  _getRightChild(index) {
+    return this._items[this._getRightChildIndex(index)]
+  }
+  _getLeftChild(index) {
+    return this._items[this._getLeftChildIndex(index)]
+  }
+  _getParent(index) {
+    return this._items[this._getParentIndex(index)]
+  }
+  peek() {
+    return this._items[this._items.length - 1]
+  }
   push(value) {
-    this.array.push(value)
+    this._items.push(value)
   }
-
   pop() {
-    return this.array.pop()
+    return this._items.pop()
   }
-
 }
-
-module.exports = Heap
--- heap.test.js
+++ heap.test.js
@@ -1,12 +1,40 @@
-const { expect } = require('chai')
-const Heap = require('./heap')
+import { expect } from 'chai'
+import Heap from './heap'
 
-describe.only('Heap', () => {
-  describe('Push', () => {
-    it('can push a value', () => {
-      const heap = new Heap()
-      heap.push(5)
-      expect(heap.pop()).to.deep.equal(5)
+describe('Heap', () => {
+
+  describe('push(value)', () => {
+
+    context('when the heap is empty', () => {
+
+      it('adds the value to the heap', () => {
+        const heap = new Heap()
+        heap.push(5)
+        expect(heap.peek()).to.equal(5)
+      })
+
     })
+
+    context('when the heap contains an item greater than value', () => {
+
+      it('adds the value to the top of the heap', () => {
+        const heap = new Heap(6)
+        heap.push(5)
+        expect(heap.peek()).to.equal(5)
+      })
+
+    })
+
+    context('when the heap contains an item less than value', () => {
+
+      it('add the value to the bottom of the heap', () => {
+        const heap = new Heap(6)
+        heap.push(7)
+        expect(heap.peek()).to.equal(6)
+      })
+
+    })
+
   })
+
 })
