d8047dba8a899c1c02027017e152d9ed9df390ea
thebearingedge <contact@timdav.is>
2018-12-21 19:44:12 -0800

    Begin implementing rebalancing algorithms.

--- red-black-tree.js
+++ red-black-tree.js
@@ -1,46 +1,57 @@
-class Node {
-  constructor(value, parent, left = null, right = null, isBlack = false) {
+export class Node {
+  constructor(value, parent, isBlack = false, left = null, right = null) {
     this.left = left
     this.value = value
     this.right = right
     this.parent = parent
     this.isBlack = isBlack
   }
+  find(value) {
+    let node = this
+    let parent = node.parent
+    while (node) {
+      if (value === node.value) break
+      parent = node
+      if (value < node.value) node = node.left
+      else node = node.right
+    }
+    return { parent, node }
+  }
+  toJSON() {
+    const { value, isBlack, left, right } = this
+    return {
+      value,
+      isBlack,
+      left: left && left.toJSON(),
+      right: right && right.toJSON()
+    }
+  }
 }
 
 export default class RedBlackTree {
-  constructor() {
+  constructor(...values) {
     this._root = null
+    return values.reduce((tree, value) => tree.insert(value), this)
   }
-
   insert(value) {
     if (this._root === null) {
-      this._root = new Node(value, this._root)
+      this._root = new Node(value, this._root, true)
       return this
     }
-    let parent = this._root
-    while (parent !== null) {
-      if (parent.value === value) return this
-
-      if (parent.value < value && parent.right === null) {
-        parent.right = new Node(value, parent)
-        return this
-      }
-      else if (parent.value < value) {
-        parent = parent.right
-      }
-      else {
-        if (parent.left === null) {
-          parent.left = new Node(value, parent)
-          return this
-        }
-        parent = parent.left
-      }
-      return this
+    let { parent, node } = this._root.find(value)
+    if (!node) {
+      value < parent.value
+        ? parent.left = new Node(value, parent)
+        : parent.right = new Node(value, parent)
     }
+    return this
   }
-
   contains(value) {
-    return this._root === value
+    return !!this._root &&
+           !!this._root.find(value).node
+  }
+  toJSON() {
+    if (!this._root) return null
+    return this._root.toJSON()
   }
 }
--- red-black-tree.test.js
+++ red-black-tree.test.js
@@ -2,18 +2,89 @@ import RedBlackTree from './red-black-tree'
 import { expect } from 'chai'
 
 describe.only('Red Black Tree', () => {
-  describe('insert', () => {
+
+  describe('insert(value)', () => {
+
     it('should be able to insert a root node', () => {
       const tree = new RedBlackTree()
       tree.insert(5)
-      expect(tree.contains(5)).to.equal(true)
+      expect(tree.toJSON()).to.deep.equal({
+        value: 5,
+        left: null,
+        right: null,
+        isBlack: true
+      })
     })
 
     it('should be able to insert multiple nodes', () => {
       const tree = new RedBlackTree()
-      tree.insert(5, 6)
-      expect(tree.contains(5)).to.equal(true)
-      expect(tree.contains(6)).to.equal(true)
+      tree
+        .insert(5)
+        .insert(6)
+      expect(tree.toJSON()).to.deep.equal({
+        value: 5,
+        left: null,
+        isBlack: true,
+        right: {
+          value: 6,
+          left: null,
+          right: null,
+          isBlack: false
+        }
+      })
     })
+
+    it('should rebalance in-order insertions', () => {
+      const tree = new RedBlackTree(1, 2, 3)
+      expect(tree.toJSON()).to.deep.equal({
+        value: 2,
+        isBlack: true,
+        left: {
+          value: 1,
+          isBlack: false,
+          left: null,
+          right: null
+        },
+        right: {
+          value: 3,
+          isBlack: false,
+          left: null,
+          right: null
+        }
+      })
+    })
+
   })
+
+  describe('contains(value)', () => {
+
+    context('when the tree is empty', () => {
+
+      it('returns false', () => {
+        const tree = new RedBlackTree()
+        expect(tree.contains(5)).to.equal(false)
+      })
+
+    })
+
+    context('when the tree does not contain the value', () => {
+
+      it('returns false', () => {
+        const tree = new RedBlackTree(1, 2, 3, 4)
+        expect(tree.contains(5)).to.equal(false)
+      })
+
+    })
+
+    context('when the tree contains the value', () => {
+
+      it('returns true', () => {
+        const tree = new RedBlackTree(1, 2, 3, 4)
+        expect(tree.contains(4)).to.equal(true)
+      })
+
+    })
+
+  })
+
 })
