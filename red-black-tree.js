4542363feea33b8f8888b4f4fb8ac2eba008f4fd
MouseZero <rm@mousezero.com>
2018-12-21 20:56:02 -0800

    add getters and setters for children

--- red-black-tree.js
+++ red-black-tree.js
@@ -1,10 +1,24 @@
 export class Node {
-  constructor(value, parent, isBlack = false, left = null, right = null) {
-    this.left = left
+  constructor(value, isBlack = false) {
+    this._left = null
     this.value = value
-    this.right = right
-    this.parent = parent
+    this._right = null
+    this.parent = null
     this.isBlack = isBlack
+  }
+  get left() {
+    return this._left
+  }
+  set left(node) {
+    this._left = node
+    if (node) node.parent = this
+  }
+  get right() {
+    return this._right
+  }
+  set right(node) {
+    this._right = node
+    if (node) node.parent = this
   }
   find(value) {
     let node = this
@@ -26,6 +40,26 @@ export class Node {
       right: right && right.toJSON()
     }
   }
+  get grandparent() {
+    if (this.parent === null || this.parent.parent === null) return null
+    return this.parent.parent
+  }
+  get uncle() {
+    if (this.parent === null || this.parent.parent === null) return null
+    const grandparent = this.parent.parent
+    if (grandparent.left === this.parent) {
+      return grandparent.right
+    }
+    else {
+      return grandparent.left
+    }
+  }
+  get isRed() {
+    return !this.isBlack
+  }
+  set isRed(isRed) {
+    this.isBlack = !isRed
+  }
 }
 
 export default class RedBlackTree {
@@ -35,14 +69,21 @@ export default class RedBlackTree {
   }
   insert(value) {
     if (this._root === null) {
-      this._root = new Node(value, this._root, true)
+      this._root = new Node(value, true)
       return this
     }
     let { parent, node } = this._root.find(value)
     if (!node) {
       value < parent.value
-        ? parent.left = new Node(value, parent)
-        : parent.right = new Node(value, parent)
+        ? node = parent.left = new Node(value)
+        : node = parent.right = new Node(value)
+    }
+    else return this // We don't insert duplicates
+    if (!node.grandparent) return this
+    if (node.uncle === null || node.uncle.isBlack) {
+      node.parent.isBlack = node.grandparent.isBlack
+      node.grandparent.isBlack = !node.parent.isBlack
+
     }
     return this
   }
